54e5f3628e29ea31c4cca6228e66906c3b6a0012
Ismail Elmaliki <check4elmaliki@live.com>
2020-06-22 09:16:12 -0400

    Initial Commit

---
+++ IsmailCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IsmailCard
+//
+//  Created by Ismail Elmaliki on 6/22/20.
+//  Copyright © 2020 Ismail Elmaliki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
