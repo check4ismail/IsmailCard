c4d89e4fd61580dd09f4f4a82b53f796d1047cc3
Ismail Elmaliki <check4elmaliki@live.com>
2020-06-22 09:51:43 -0400

    Using SwiftUI to create a virtual business card

--- IsmailCard/ContentView.swift
+++ IsmailCard/ContentView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+		ZStack {
+			Color(.systemTeal)
+				.edgesIgnoringSafeArea(.all)
+			VStack {
+				Image("ismail")
+					.resizable().aspectRatio(contentMode: .fit)
+					.frame(width: 150, height: 150)
+					.clipShape(Circle())
+					.overlay(
+						Circle().stroke(Color.white, lineWidth: 5)
+					)
+				Text("Ismail Elmaliki")
+					.font(Font.custom("Pacifico-Regular", size: 40))
+					.bold()
+					.foregroundColor(.white)
+				
+				Text("iOS Developer")
+					.foregroundColor(.white)
+					.font(.system(size: 25))
+				Divider()
+				
+				Rectangle()
+				.frame(height: 50)
+				.cornerRadius(25)
+				.foregroundColor(.white)
+				.overlay(
+					Text("+1 123-456-7890")
+				)
+			}
+		}
     }
 }
 
